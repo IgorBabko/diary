81b0b81aa6db71172c55f875efd5c073a293506e
emerge <i.i.babko@gmail.com>
2016-05-17 20:02:54 +0300

    add show-note component

---
+++ dev/notes/show-note.component.ts
@@ -0,0 +1,20 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'show-note',
+    template: `
+        <div>
+            <form action="#">
+                <div>
+                    <label for="text">Text:</label><br>
+                    <textarea id="text"></textarea>
+                </div>
+                <button type="submit">Save</button>
+            </form>
+            <button>Edit</button>
+        </div>
+    `
+})
+export class EditNoteComponent {
+
+}
