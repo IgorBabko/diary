7e013e71401d6e075a2c57fc4a5f34183df92fe3
emerge <i.i.babko@gmail.com>
2016-05-18 15:24:19 +0300

    add FORM_DIRECTIVES to the root component

--- src/boot.ts
+++ src/boot.ts
@@ -1,3 +1,4 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { FORM_DIRECTIVES } from '@angular/common'
 import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+bootstrap(AppComponent, [FORM_DIRECTIVES]);
--- src/diary/notes/add-note.component.ts
+++ src/diary/notes/add-note.component.ts
@@ -1,9 +1,22 @@
-import {Component} from "@angular/core";
+import {Component OnInit} from "@angular/core";
+import {ControlGroup} from '@angular/common'
 
 @Component({
     selector: 'o-add-note',
     templateUrl: 'templates/diary/notes/add-note.tpl.html'
 })
-export class AddNoteComponent {
+export class AddNoteComponent implements OnInit {
+    addNoteForm: ControlGroup;
 
+    constructor(private addNoteForm: ControlGroup) {
+
+    }
+
+    ngOnInit():any {
+        this.addNoteForm.
+    }
+
+    onSubmit() {
+
+    }
 }
