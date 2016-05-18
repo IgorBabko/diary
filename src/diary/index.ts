d41a6595c018ccd7f5c48c401f63b3cb532a3437
emerge <i.i.babko@gmail.com>
2016-05-18 14:16:33 +0300

    add delete note modal

--- src/diary/index.ts
+++ src/diary/index.ts
@@ -2,10 +2,11 @@ import {Component} from '@angular/core'
 import {NoteListComponent} from './notes/note-list.component';
 import {AddNoteComponent} from './notes/add-note.component';
 import {EditNoteComponent} from './notes/edit-note.component';
+import {DeleteNoteComponent} from './notes/delete-note.component';
 
 @Component({
     selector: 'diary',
     templateUrl: 'templates/diary/index.tpl.html',
-    directives: [NoteListComponent, AddNoteComponent, EditNoteComponent]
+    directives: [NoteListComponent, AddNoteComponent, EditNoteComponent, DeleteNoteComponent]
 })
 export class DiaryComponent {}
---
+++ src/diary/notes/delete-note.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'o-delete-note',
+    templateUrl: 'templates/diary/notes/delete-note.tpl.html'
+})
+export class DeleteNoteComponent {
+
+}
