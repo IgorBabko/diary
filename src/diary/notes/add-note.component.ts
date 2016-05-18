29f023be879692c7a9df10971ebe98e65bebfa56
emerge <i.i.babko@gmail.com>
2016-05-18 12:45:56 +0300

    add add-note button

---
+++ src/diary/notes/add-note.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'o-add-note',
+    templateUrl: 'templates/diary/notes/add-note.tpl.html'
+})
+export class AddNoteComponent {
+
+}
--- src/diary/notes/note-list.component.ts
+++ src/diary/notes/note-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core'
 import {NoteService} from './note.service';
 import {NoteComponent} from './note.component';
+import {AddNoteComponent} from './add-note.component';
 
 @Component({
     selector: 'o-note-list',
     templateUrl: 'templates/diary/notes/note-list.tpl.html',
     providers: [NoteService],
-    directives: [NoteComponent]
+    directives: [AddNoteComponent, NoteComponent]
 })
 export class NoteListComponent {
     constructor(private _noteService: NoteService) {}
